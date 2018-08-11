d74d015f097c608c1c5779737182d07899f11fcb
Fiona Wilson <fionaw237@gmail.com>
2018-08-11 15:15:10 +0100

    Adds CRUD methods for studios table

--- models/studio.rb
+++ models/studio.rb
@@ -8,4 +8,29 @@ class Studio
     @name = options['name']
   end
 
+  def save()
+    sql = 'INSERT INTO studios (name) VALUES ($1) RETURNING id'
+    values = [@name]
+    result = SqlRunner.run(sql, values).first()
+    @id = result['id'].to_i()
+  end
+
+  def update()
+    sql = 'UPDATE studios SET name = $1 WHERE id = $2'
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = 'DELETE FROM studios WHERE id = $1'
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = 'DELETE FROM studios'
+    SqlRunner.run(sql)
+  end
+
+
 end
