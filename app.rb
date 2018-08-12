831b484bd426851c0eeb951355ca9c450e0be206
Fiona Wilson <fionaw237@gmail.com>
2018-08-12 12:31:15 +0100

    Adds route to homepage

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require("sinatra")
 require("sinatra/contrib/all")
 require("pry-byebug")
-
-require_relative("models/gymclass.rb")
-# require_relative("models/house.rb")
+# require_relative('controllers/classes_controller')
+# require_relative('controllers/members_controller')
+require_relative('controllers/sessions_controller')
+require_relative("models/session.rb")
 also_reload("models/*")
 
 get '/gym' do
-  @gymclasses = GymClass.all()
+  @sessions = Session.all()
   erb(:index)
 end
