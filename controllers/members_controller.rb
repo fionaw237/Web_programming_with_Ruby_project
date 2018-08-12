b74ad5f2b8b795d0e8706176bc597909ad1d3898
Fiona Wilson <fionaw237@gmail.com>
2018-08-12 18:38:43 +0100

    Adds routes for adding a new member

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -10,6 +10,23 @@ get '/gym/members' do
   erb(:"members/index")
 end
 
+#new
+get '/gym/members/new' do
+  erb(:"members/new")
+end
+
+#create
+post '/gym/members' do
+  @member = Member.new(params)
+  @member.save()
+  redirect "/gym/members"
+end
+
+get '/gym/members/:id' do
+  @member = Member.find(params[:id])
+  erb(:"members/show")
+end
+
 get '/gym/members/:id' do
   @member = Member.find(params[:id])
   erb(:"members/show")
